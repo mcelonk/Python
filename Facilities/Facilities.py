6ae8f81fc2551f7fc6a34bc154d08bc4edb4bef5
mcelonk <marek.celonk@seznam.cz>
2020-02-16 02:05:08 +0100

    verze 0.2

--- Facilities/Facilities.py
+++ Facilities/Facilities.py
@@ -1,9 +1,11 @@
 import numpy as np
+import math
+
 
 class Facility:
-    def __init__(self ,c ,m):
-        self.c = c #centroid
-        self.m = m #radius
+    def __init__(self, c, m):
+        self.c = c  # centroid
+        self.m = m  # radius
 
 
 # nacteni do 2d pole
@@ -12,58 +14,61 @@ def readXYZ(fil):
     XYZ = open(fil)
     for line in XYZ:
         l = line.split()
-        p_list.append(l)
+        float_l = [float(x) for x in l]
+
+        p_list.append(float_l)
+
     return p_list
 
-xyz = readXYZ("test_15k_bez_duplict.xyz")
 
-tresh = 5 #cena (cost, hranicni hodnota)
-alpha = 9.5 #koeficient
+# funkce square distance, jeste nefunguje, problem s datovym typem
+def countDist(p1, p2):
+    distance = math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2 + (p2[2] - p1[2]) ** 2)
+    return distance
+
+
+def nearestFacility(p1, facility_list):
+    min = countDist(p1, facility_list[0].c)
+    min = min + 2 * facility_list[0].m
+    for z in facility_list:
+        distance = countDist(p1, z.c)
+        distance = distance + 2 * z.m
+        if distance < min:
+            min = distance
+    return min
+
+filename = "test_15k_bez_duplict.xyz"
+xyz = readXYZ(filename)
+
+tresh = 5  # cena (cost, hranicni hodnota)
+alpha = 9.5  # koeficient
 f_list = []
 f = Facility(xyz[0], 0)  # vlozeni prvni facility do listu
 f_list.append(f)
 
-
 for i in xyz:
-    p1 = np.array([i[0], i[1], i[2]])
-    p1 = p1.astype(float)
-    dis_list = [] #pole vzdalenosti bodu od facilities
+    # vypocet vzdalenosti s kazdou existujici facilitou
+    # index_dist_min = dis_list.index(min(dis_list)) index pro nejblizsi facility, zatim se nevyuziva
+    g = nearestFacility(i, f_list)
+    p = np.random.uniform()  # vygenerovani pravdepodobnosti
 
-    #vypocet vzdalenosti s kazdou existujici facilitou
-    for z in f_list:
-        p2 = np.array([z.c[0], z.c[1], z.c[2]])
-        p2 = p2.astype(float)
-        squared_dist = np.sum((p1 - p2) ** 2, axis=0)
-        dist = np.sqrt(squared_dist) #vypocitani vzdalenosti
-        dist = dist + 2*z.m #nasobeni 2m jako v clanku
-        dis_list.append(dist) #pridat do listu vzdalenosti
+    # rozhodnuti zda ma byt vytvorena nova facility
+    if p < g / (tresh * alpha):
+        m = (g / (tresh * alpha)) / 6
+        f = Facility(i, m)
 
-    #index_dist_min = dis_list.index(min(dis_list)) index pro nejblizsi facility, zatim se nevyuziva
-    g = min(dis_list)
-    p = np.random.uniform() #vygenerovani pravdepodobnosti
+        # pokud je nejaka facility prilis blizko, bude odstranena
+        for facility in f_list:
+            dist = countDist(i, facility.c)
+            if dist < facility.m:
+                f_list.remove(facility)
 
-    #rozhodnuti zda ma byt vytvorena nova facility
-    if p > g/(tresh*alpha):
-        g_u = g #zapamatuje si posledni hodnotu g, vyuziva se dale pro vypocet m(w)
-        continue #nevytvari se nova facility, posun na dalsi bod
+        f_list.append(f)  # pridani facility do listu
 
-    #vytvoreni facility w
-    m = (g/(tresh*alpha))/6
-    f = Facility(i, m)
 
-    #pokud je nejaka facility prilis blizko, bude odstranena
-    for z in f_list:
-        p2 = np.array([z.c[0], z.c[1], z.c[2]])
-        p2 = p2.astype(float)
-        squared_dist = np.sum((p1 - p2) ** 2, axis=0)
-        dist = np.sqrt(squared_dist)  # vypocitani vzdalenosti
-        if dist < z.m:
-            f_list.remove(z)
 
-    f_list.append(f) #pridani facility do listu
-
-#vytvoreni csv souboru
-out = open("facilities.csv", "w")
+# vytvoreni csv souboru
+out = open(filename[:-4] + "_test.csv", "w")
 out.write("X")
 out.write(" ")
 out.write("Y")
@@ -71,12 +76,9 @@ out.write(" ")
 out.write("Z")
 out.write("\n")
 for k in f_list:
-    out.write(k.c[0])
+    out.write(str(k.c[0]))
     out.write(" ")
-    out.write(k.c[1])
+    out.write(str(k.c[1]))
     out.write(" ")
-    out.write(k.c[2])
+    out.write(str(k.c[2]))
     out.write("\n")
-
-
-
